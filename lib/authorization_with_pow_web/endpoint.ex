498bff0b95d34686d4fdab2c996959667425d9b2
Anthony Gonzalez <boilercoding@gmail.com>
2020-07-20 18:28:30 -0400

    added remember to login form

--- lib/authorization_with_pow_web/endpoint.ex
+++ lib/authorization_with_pow_web/endpoint.ex
@@ -51,5 +51,6 @@ defmodule AuthorizationWithPowWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
   plug Pow.Plug.Session, otp_app: :authorization_with_pow
+  plug PowPersistentSession.Plug.Cookie
   plug AuthorizationWithPowWeb.Router
 end
